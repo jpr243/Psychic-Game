068e2c6bf093111bab49b6d1469dcdb296b70cf1
Janet R <jpr.243@outlook.com>
2019-10-10 20:30:29 +0800

    Index file and jsgame file code written

---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+var choiceLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+var youWin = 0;
+var youLose = 0;
+var guessesLeft = 10;
+var guessedLetters = []; //Array to capture user letter input
+document.onkeyup = function(event) {
+  var yourGuess = event.key; //variable to capture user's guess
+  guessedLetters.push(yourGuess); //This is to capture letters entered by the user
+  var computerAns =
+    choiceLetters[Math.floor(Math.random() * choiceLetters.length)];
+  //This outlines the rules of the game.  I still need to code clearing letters after youWin and youLose
+  if (yourGuess === computerAns) {
+    youWin++;
+    guessesLeft = 9;
+  } else if (guessesLeft === 0) {
+    youLose++;
+    guessesLeft = 9;
+  } else {
+    guessesLeft--;
+  }
+  //This is the html
+  var html =
+    "<p><strong>Guess what letter I'm thinking of:</strong></p>" +
+    "<p>Wins: " +
+    youWin +
+    "</p>" +
+    "<p>Losses: " +
+    youLose +
+    "</p>" +
+    "<p>Guesses Remaining: " +
+    guessesLeft +
+    "</p>" +
+    "<p>Guessed Letter: " +
+    guessedLetters.join(", ") +
+    "</p>"; //Show guessed letters heredocument.querySelector("#psychicGame").innerHTML = html;
+
+  document.getElementById("results").innerHTML = html;
+};
